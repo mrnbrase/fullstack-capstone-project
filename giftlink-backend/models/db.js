24d3b25cf8e498159f47163993aea7b74adceb5d
mrnbrase <mrnbrase@hotmail.com>
2024-05-19 11:38:06 -0400

    Implemented changes

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,32 +1,23 @@
+// db.js
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
 // MongoDB connection URL with authentication options
-const url = process.env.MONGO_URL;
-const dbName = "giftdb";
+let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
+const dbName = "giftdb";
 
 async function connectToDatabase() {
     if (dbInstance){
-        return dbInstance;
-    }
+        return dbInstance
+    };
 
-    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(url);
 
-    try {
-        // Task 1: Connect to MongoDB
-        await client.connect();
-
-        // Task 2: Connect to database giftDB and store in variable dbInstance
-        dbInstance = client.db(dbName);
-
-        // Task 3: Return database instance
-        return dbInstance;
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        throw error;
-    }
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
